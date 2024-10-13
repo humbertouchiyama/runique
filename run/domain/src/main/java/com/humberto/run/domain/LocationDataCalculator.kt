77bebc526ba1c7bff1c0d0b1a3168350da8006b2
humbertocosta <humbertou.costa@gmail.com>
2024-10-13 17:41:39 -0300

    sets up running tracker

---
+++ run/domain/src/main/java/com/humberto/run/domain/LocationDataCalculator.kt
@@ -0,0 +1,16 @@
+package com.humberto.run.domain
+
+import com.humberto.core.domain.location.LocationTimestamp
+import kotlin.math.roundToInt
+
+object LocationDataCalculator {
+
+    fun getTotalDistanceMeters(locations: List<List<LocationTimestamp>>): Int {
+        return locations
+            .sumOf { timestampsPerLine ->
+                timestampsPerLine.zipWithNext { location1, location2 ->
+                    location1.location.location.distanceTo(location2.location.location)
+                }.sum().roundToInt()
+            }
+    }
+}
