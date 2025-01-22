fc648794b240e65ddaa0b590bbdfbb380305fd50
humbertocosta <humbertou.costa@gmail.com>
2025-01-22 16:15:14 -0300

    implements coroutine test extension

--- auth/domain/src/test/kotlin/com/humberto/auth/domain/UserDataValidatorTest.kt
+++ auth/domain/src/test/kotlin/com/humberto/auth/domain/UserDataValidatorTest.kt
@@ -11,7 +11,7 @@ class UserDataValidatorTest {
     private lateinit var userDataValidator: UserDataValidator
 
     @BeforeEach
-    fun setup() {
+    fun setUp() {
         userDataValidator = UserDataValidator(
             patternValidator = object : PatternValidator {
                 override fun matches(value: String): Boolean {
